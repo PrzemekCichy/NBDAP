bba80e419d46116d753b7c4e8310f72660db4613
Przemek Cichy <przemek.cichy@outlook.com>
2018-02-16 02:43:20 +0000

    Set up basic controller to get and return files llist from location

---
+++ WebApp/Controllers/FilesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class FilesController: Controller
+    {
+        [HttpGet("FilesController/getFiles")]
+        public JsonResult GetFiles() {
+            return new JsonResult(new List<object> {
+               new  { name = "Test.json", path = "C:/Desktop" },
+               new  { name = "Test2.json", path = "C:/Desktop/New" },
+            });
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -15,6 +15,7 @@ namespace WebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,8 +26,11 @@ namespace WebApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMvc();
+
             app.Run(async (context) =>
             {
+                //throw new Exception("lmao");
                 await context.Response.WriteAsync("Hello World!");
             });
         }
